11ae8e25798151193f5f1d2a12691c2cf3222de7
Davide Zavattero <davide.zavattero@sysdata.it>
2022-08-04 03:18:51 +0200

    progress

--- challenge-angular/src/app/services/auth/auth.service.ts
+++ challenge-angular/src/app/services/auth/auth.service.ts
@@ -1,11 +1,50 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiEndpoints } from '../../enums/api-endpoints';
+import { UserProfile } from '../../interfaces/user-profile';
+import { ApiUrlService } from '../api-url/api-url.service';
+import { HttpService } from '../http/http.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
-  isUserLoggedIn() {
-    return false;
+  private token!: unknown;
+
+  constructor(
+    private apiUrlService: ApiUrlService,
+    private httpService: HttpService
+  ) {}
+  isUserLoggedIn(): boolean {
+    const usr = this.getUserObj();
+    return typeof usr === 'object';
+  }
+
+  getUserObj(): unknown {
+    const token = this.getToken();
+    if (typeof token === 'string') {
+      const usrResponse = this.getUserAsync(token);
+      console.log(usrResponse);
+    }
+    return null;
+  }
+
+  getToken() {
+    return this.token || localStorage.getItem('token');
+  }
+
+  async getUserAsync(token: string) {
+    const resolved = (await new Promise((resolve, reject) => {
+      this.getUser(token).subscribe((res) => {
+        resolve(res);
+      });
+    })) as UserProfile;
+    console.log('aaaaaaaaaa', resolved);
+    return resolved;
+  }
+
+  getUser(token: string): Observable<UserProfile> {
+    const url = this.apiUrlService.getUrl(ApiEndpoints.PROFILE);
+    return this.httpService.get(url, token) as Observable<UserProfile>;
   }
 }
--- challenge-angular/src/app/services/http/http.service.ts
+++ challenge-angular/src/app/services/http/http.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpService {
+  constructor(private authService: AuthService, private http: HttpClient) {}
 
-  constructor() { }
+  setHeaders(token: string): HttpHeaders {
+    const headers = new HttpHeaders();
+    headers.set('token', token);
+    return headers;
+  }
+
+  get(url: string, token: string): Observable<unknown> {
+    return this.http.get(url, { headers: this.setHeaders(token) });
+  }
 }
